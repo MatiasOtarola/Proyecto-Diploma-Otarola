9ab3ffa2f7c3195e1aa581a761e78a276e74ad52
MatiasOtarola <matias.e.otarola@gmail.com>
2018-11-23 05:54:25 -0300

    Proyecto 1.0

---
+++ Directorio/WindowsApplication2/Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+Imports System
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim directorio As New DirectoryInfo("c:\users")
+        Dim subdirectorio As DirectoryInfo() = directorio.GetDirectories()
+        Dim subcarpetas As DirectoryInfo
+        For Each subcarpetas In subdirectorio
+            ListBox1.Items.Add(subcarpetas.Name.ToString)
+        Next
+        Dim cantidad As String = ListBox1.Items.Count
+        MsgBox(CStr(cantidad))
+
+    End Sub
+End Class
